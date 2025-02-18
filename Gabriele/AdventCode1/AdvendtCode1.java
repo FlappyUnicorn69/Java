acc2a54897bdb88161ff868da8367ab5d501556e
Gabriele Basello <gabas2000@gmail.com>
2025-02-18 15:33:04 +0100

    Rehab new files

---
+++ Gabriele/AdventCode1/AdvendtCode1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.Collection;
+
+public class AdvendtCode1 {
+ public static void main(String[] args) {
+    
+    List<Integer> rightList = new ArrayList<>();
+    List<Integer> leftList = new ArrayList<>();
+
+
+  int totDist = 0;
+  if (int = i; i <= leftList.size(); i++) {
+    
+    totDist += Math.abs(leftList[i] - leftList[i]);
+   }
+   System.out.println("The value is: ");
+ }
+}
