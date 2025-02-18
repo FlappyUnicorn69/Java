51ff28dea6494fc05570de7847d9d9736149574b
Gabriele Basello <gabas2000@gmail.com>
2025-02-18 15:14:38 +0100

    Add new files

---
+++ Gabriele/AdvendtCode1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class AdvendtCode1 {
+ public static void main(String[] args) {
+    int[] leftArray = {};
+    int[] rightArray = {};
+    
+  int totDist = 0;
+  if (int = i; i < Array.size(); i++) {
+    
+    totDist += Math(leftArray[i] - rightArray[i]);
+   }
+   System.out.println("The value is: ");
+ }
+}
